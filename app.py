fb4dc0a03f0ee37444274afb4ca268d701d978e2
Nathan Olborski <nathan.olborski@imt-atlantique.net>
2021-10-22 10:50:12 +0200

    tests jinja et json

--- app.py
+++ app.py
@@ -1,35 +1,21 @@
 from flask import Flask
 from flask import render_template
 from database.database import db, init_database
-
+import json
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database/database.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db.init_app(app) # (1) flask prend en compte la base de donnee
-with app.test_request_context(): # (2) bloc execute a l'initialisation de Flask
- init_database()
+db.init_app(app)  # (1) flask prend en compte la base de donnee
+with app.test_request_context():  # (2) bloc execute a l'initialisation de Flask
+    init_database()
 
 
-
-@app.route('/')
+@app.route('/index', methods = ['POST', 'GET'])
+@app.route('/', methods = ['POST', 'GET'])
 def index():
-    return render_template("index.html")
-
-
-@app.route('/view/a')
-def view_a():
-    return render_template("homepage.jinja2")
-
-
-@app.route('/view/b')
-def view_b():
-    return render_template("template_b.html.jinja2")
-
-
-@app.route('/view/c')
-def view_c():
-    return render_template("template_c.html.jinja2")
+    jsonfile = "{rice: 5, yam: 6, tomato: 0, potato: 0,beans: 0, maize: 0, oil: 3}"
+    return render_template("index.html",jsonfile=jsonfile, test='jpp')
 
 
 if __name__ == '__main__':
