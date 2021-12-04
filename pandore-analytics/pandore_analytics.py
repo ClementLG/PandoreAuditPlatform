341e91cadef24e9449f5396ea22db99cb8c29af2
Clement LE GRUIEC <clementlegruiec@gmail.com>
2021-12-04 22:53:48 +0100

    update

--- pandore-analytics/pandore_analytics.py
+++ pandore-analytics/pandore_analytics.py
@@ -18,38 +18,37 @@ class PandoreAnalytics:
         print("Pandore analytics is running")
 
     def analyse_ip_dns(self, ip, dns=None):
-        service = None
-        # 1st try to parse the dns
+
         if dns is not None:
-            dns_info = whois(dns)
+            # 1st try to parse the dns
             for key in SERVICES_DICTIONARY:
                 # 1st try to parse the dns
                 if any(srvc in dns for srvc in SERVICES_DICTIONARY[key]):
-                    print("Service found for "+dns+" : "+key)
+                    print("Service found for " + dns + " : " + key)
                     return key
 
-        # 2nd to find the service using the DNS lookup
-        if dns is not None:
-            dns_info = whois(dns)
-            for key in SERVICES_DICTIONARY:
-                if any(srvc in dns_info["org"].lower() for srvc in SERVICES_DICTIONARY[key]):
-                    print("Service found for "+dns+" : "+key)
-                    return key
+            # 2nd to find the service using the DNS lookup
+            try:
+                dns_info = whois(dns)
+                for key in SERVICES_DICTIONARY:
+                    if any(srvc in dns_info["org"].lower() for srvc in SERVICES_DICTIONARY[key]):
+                        print("Service found for " + dns + " : " + key)
+                        return key
+            except:
+                pass
 
         # 3rd to find the service using the IP lookup
-        ip_info = IPWhois(ip).lookup_rdap()
-        for key in SERVICES_DICTIONARY:
-            if any(srvc in ip_info['asn_description'].lower() for srvc in SERVICES_DICTIONARY[key]):
-                print("Service found for "+ip+" : "+key)
-                return key
+        try:
+            ip_info = IPWhois(ip).lookup_rdap()
+            for key in SERVICES_DICTIONARY:
+                if any(srvc in ip_info['asn_description'].lower() for srvc in SERVICES_DICTIONARY[key]):
+                    print("Service found for " + ip + " : " + key)
+                    return key
+        except:
+            pass
 
+        return None
 
     def list_services(self):
         for key in SERVICES_DICTIONARY:
             print(key)
-
-        # obj = IPWhois(ip)
-        # results = obj.lookup_rdap(depth=1)
-        # print(results)
-        # info2 = whois("legruiec.fr")
-        # print(info2)
--- pandore-analytics/test.py
+++ pandore-analytics/test.py
@@ -2,4 +2,5 @@ from pandore_analytics import PandoreAnalytics
 
 analytics = PandoreAnalytics()
 
-analytics.analyse_ip_dns('8.8.8.8', "tiktok.com")
+analytics.analyse_ip_dns('8.8.8.8', "google.com")
+analytics.analyse_ip_dns('8.8.8.8')
