cfbd1c19d4353103b63715745837985cdb2ba25b
Clement LE GRUIEC <clementlegruiec@gmail.com>
2022-01-08 00:47:30 +0100

    enable swagger UI

--- pandore-sniffer/app/gui/__init__.py
+++ pandore-sniffer/app/gui/__init__.py
@@ -4,14 +4,28 @@
 from time import sleep
 
 from flask import Flask, jsonify, Request, render_template, Response
-from flask_swagger import swagger
+from flask_swagger_ui import get_swaggerui_blueprint
 from app import pandore_sniffer
+from app.pandore_config import *
 import datetime
-
 
 # FLASK APP=====================================================================
 
 app = Flask(__name__)
+
+# SWAGGER=======================================================================
+SWAGGER_URL = '/api'
+API_URL = '/static/swagger/swagger.json'
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Pandore Sniffer API"
+    }
+)
+app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
+
+# FLASK ROUTES===================================================================
 
 
 @app.route("/", methods=["GET"])
@@ -19,19 +33,13 @@ app = Flask(__name__)
 def index():
     return render_template("logging/logging.html")
 
-@app.route("/api/spec", methods=["GET"])
-def spec():
-    swag = swagger(app)
-    swag['info']['version'] = "1.0"
-    swag['info']['title'] = "Sniffer API"
-    swag['info']['maintainer'] = "Clement LE GRUIEC"
-    return jsonify(swag)
 
 @app.route("/log_stream", methods=["GET"])
 def stream():
     return Response(flask_logger(), mimetype="text/plain", content_type="text/event-stream")
 
-# FUNCTIONS
+
+# Functions=========================================================================
 def flask_logger():
     for i in range(100):
         current_time = datetime.datetime.now().strftime('%H:%M:%S') + "\n"
